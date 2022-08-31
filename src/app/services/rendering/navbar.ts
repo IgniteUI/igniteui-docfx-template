2b6a0b77861d8ee595544eee4dfed0f78dbd9367
Volen99 <volen1999@gmail.com>
2022-08-31 10:30:09 +0300

    add hello bar

--- src/app/services/rendering/navbar.ts
+++ src/app/services/rendering/navbar.ts
@@ -1,5 +1,5 @@
-import util from "../utils";;
-import { RenderingService } from "../../types";;
+import util from "../utils";
+import { RenderingService } from "../../types";
 
 export class NavbarRenderingService extends RenderingService {
 
@@ -9,6 +9,8 @@ export class NavbarRenderingService extends RenderingService {
 
     public render() {
         this.renderNavbar();
+        this.changeHelloBarContent(location.pathname);
+        this.attachCloseBtnEvent();
     }
 
     private renderNavbar() {
@@ -74,6 +76,67 @@ export class NavbarRenderingService extends RenderingService {
                 }
             });
             this.renderNavbar();
+        });
+    }
+
+    public changeHelloBarContent(currentRoute: string) {
+        let text = $('.ui-hello-bar__text');
+        let button = $('#hello-bar_cta');
+
+        let $platformMeta = $("meta[property='docfx:platform']");
+        let platform = $platformMeta.attr("content")!;
+
+        let barText = 'Want the Fastest Angular Data Grid on the Market? We’ve Got It! Quickly Bind Data with Minimal Code Involved.';
+        let barButtonText = 'Get Started';
+        let buttonHrefValue = 'https://www.infragistics.com/products/ignite-ui-angular/download';
+        let buttonXdGaLabelValue = 'Help_AngularGrids_Trial';
+        let buttonGaLabelValue = 'Hello bar – Help_AngularGrids_Trial';
+
+        if (platform === 'appbuilder') {
+            barText = 'App Builder™ Includes a Full Design System with 60+ UI Controls and Code Generation for Angular & Blazor!';
+            barButtonText = 'Start Free Trial';
+            buttonHrefValue = 'https://appbuilder.indigo.design/'
+            buttonXdGaLabelValue = 'Help_ABTrial';
+            buttonGaLabelValue = 'Hello bar – Help_ABTrial';
+
+        } else if (platform === 'blazor') {
+            if (currentRoute.includes('charts')) {
+                barText = 'Create Charts & Dashboards for Your Modern C# Blazor Web and Mobile Apps with Over 65 High-Performing Charts & Graphs in Ignite UI!';
+                barButtonText = 'Try It Free';
+                buttonHrefValue = 'https://www.infragistics.com/products/ignite-ui-blazor/download'
+                buttonXdGaLabelValue = 'Help_BlazorCharts_Trial';
+                buttonGaLabelValue = 'Hello bar – Help_BlazorCharts_Trial';
+            } else {
+                barText = 'Ignite UI for Blazor: Feature-Rich, Lightweight Tables & Data Grids to Modernize Your Apps!';
+                barButtonText = 'Start Free Trial';
+                buttonHrefValue = 'https://www.infragistics.com/products/ignite-ui-blazor/download'
+                buttonXdGaLabelValue = 'Help_BlazorGrids_Trial';
+                buttonGaLabelValue = 'Hello bar – Help_BlazorGrids_Trial';
+            }
+        } else if (platform === 'angular') {
+            if (currentRoute.includes('charts')) {
+                barText = 'Ignite UI Angular Charts: Build Expressive Dashboards and Render Data Points with 65+ Real-Time Angular Charts.';
+                barButtonText = 'Start Free Trial';
+                buttonXdGaLabelValue = 'Help_AngularCharts_Trial';
+                buttonGaLabelValue = 'Hello bar – Help_AngularCharts_Trial';
+            }
+        }
+
+        text.text(barText);
+        button.text(barButtonText);
+        button.attr('href', buttonHrefValue);
+        button.attr('data-ga-label', buttonGaLabelValue);
+        button.attr('data-xd-ga-label', buttonXdGaLabelValue);
+    }
+
+    private attachCloseBtnEvent() {
+        let anchor = $('#hello-bar-dismiss');
+        let main = $('#main');
+
+        let parent = $(anchor).parent() as unknown as JQuery<HTMLLIElement>;
+        anchor.on('click', (e) => {
+            parent.css('display','none');
+            main.css('padding-top', '124px');
         });
     }
 
--- template/conceptual.html.primary.js
+++ template/conceptual.html.primary.js
@@ -51,16 +51,20 @@ exports.transform = function (model) {
   model._isBlazor = false;
   model._isReact = false;
   model._isWC = false;
+  model._showHelloBar = false;
   if (model._platform) {
     if (model._platform === "angular") {
       model._isAngular = true;
       model._isIgnite = true;
+      model._showHelloBar = true;
     } else if (model._platform === "appbuilder") {
       model._isAppBuilder = true;
       model._isIgnite = true;
+      model._showHelloBar = true;
     } else if (model._platform === "blazor") {
       model._isBlazor = true;
       model._isIgnite = true;
+      model._showHelloBar = true;
     } else if (model._platform === "react") {
       model._isReact = true;
       model._isIgnite = true;
